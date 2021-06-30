d524c3082ddeaba03ec4fdc83a95a0a23b07c825
yangchong <yangchong211@163.com>
2021-06-30 13:00:19 +0800

    完善案例

--- app/src/main/java/com/yc/map/MainActivity.java
+++ app/src/main/java/com/yc/map/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(new Intent(this,MapsActivity.class));
                 break;
             case R.id.tv_3:
-                startActivity(new Intent(this,GoogleActivity.class));
                 break;
         }
     }
